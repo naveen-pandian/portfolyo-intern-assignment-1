bdfcae21cf52d154ebf46dd5d91ee51fbafad92b
naveen-pandian <naveenpandianp1@gmail.com>
2024-04-17 16:18:00 +0530

    production build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/portfolyo-intern-assignment-1/",
   plugins: [react()],
-  base: "/portfolyo-intern-assignment-1/"
 })
